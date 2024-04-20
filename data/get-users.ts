fce3fd1de01f95479b3ef7772820adf74fea12a5
negoescg <dan.negoescu4@gmail.com>
2024-04-20 15:00:57 +0100

    users dependencies remove

--- data/get-users.ts
+++
@@ -1,9 +0,0 @@
-import { getUsers } from '@/server/actions/user.actions';
-import { useQuery } from '@tanstack/react-query';
-
-export function useGetUsers() {
-  return useQuery({
-    queryFn: async () => getUsers(),
-    queryKey: ['users'],
-  });
-}

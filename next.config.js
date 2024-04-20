f2361d6802fe32841fb4ac2cd846f02cb4e27d43
negoescg <dan.negoescu4@gmail.com>
2024-04-20 15:21:04 +0100

    fix argon 2?

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@node-rs/argon2", "@node-rs/bcrypt"],
+  },
+};
+module.exports = nextConfig;
